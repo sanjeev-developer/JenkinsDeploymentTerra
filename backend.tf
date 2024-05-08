dab6578e9142df6de453fbd8ffcbc8ec632a496a
sanjeev-developer <sanjeevgupta9223@gmail.com>
2024-05-08 16:50:25 -0400

    all files

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "sanjeevterrastatefile"
     key            = "terraform.tfstate" # The name of the state file in the bucket
     region         = "us-west-2"
-    dynamodb_table = "sanjeev_dynamo_table_name"
+//    dynamodb_table = "sanjeev_dynamo_table_name"
   }
 }
